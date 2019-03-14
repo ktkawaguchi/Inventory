846b2c2a77b5eeae8edc7e83737e5bed20364511
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-03-13 18:46:32 -0700

    Refactored the form to be more bootstrap compatible and modified the scannerlogic to be more concise

--- scannerlogic.js
+++ scannerlogic.js
@@ -1,30 +1,15 @@
-var testdata = ['hp','dell','apple'];
+var manData = ['hp','dell','apple'];
 function hello (hello) {
     $('#new').append(hello);
 }
 
-for (var i=0; i < testdata.length; i++) {
-    
-    var labelbtn = $('<span>' + testdata[i] + '</span>');
-    labelbtn.appendTo('#new');
-
-    var radiobtn = $('<input type=radio name=' + testdata[i] + '></>');
-    radiobtn.attr('id',testdata[i]);
-    radiobtn.appendTo('#new');
+for (var i=0; i < manData.length; i++) {
 
     //creates a label for the manufactuerer and adds to form
     var label = $('<label class="form-check-label"></label>');
-    label.attr('id','Manu_'+testdata[i]).text(testdata[i]);;
-    label.appendTo('#manufacturer');
+    label.attr('id','Manu_'+manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
 
     //creates the actual input radio and adds to the form
     var manRadio = $('<input class="form-check-input" type="radio" value="checkedValue">');
-    console.log(manRadio);
-    manRadio.attr('id','radio_'+testdata[i]);
-    manRadio.appendTo('#Manu_'+testdata[i]);
+    manRadio.attr('id','radio_'+manData[i]).appendTo('#Manu_'+manData[i]);
 }
-
-
-{/* <label class="form-check-label">
-<input class="form-check-input" type="radio" name="" id="" value="checkedValue"> Display value
-</label> */}
