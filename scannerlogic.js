7015a8df8016e8f9fead5192a200304c08190733
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-03-14 13:27:08 -0700

    Changed the radio boxes to look like buttons

--- scannerlogic.js
+++ scannerlogic.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     }
 
     for (var i=0; i < manData.length; i++) {
+    //     <label class="btn btn-secondary active">
+    //     <input type="radio" name="options" id="" value="checkedValue"> Display value
+    //   </label>
 
         //creates a label for the manufactuerer and adds to form
-        var label = $('<label class="form-check-label"></label>');
+        var label = $('<label class="btn btn-secondary"></label>');
         label.attr('id','Manu_'+manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
 
         //creates the actual input radio and adds to the form
-        var manRadio = $('<input class="form-check-input" type="radio" value="checkedValue">');
+        var manRadio = $('<input name="options" autocomplete="off" type="radio" value="checkedValue">');
         manRadio.attr('id','radio_'+manData[i]).appendTo('#Manu_'+manData[i]);
     }
 
