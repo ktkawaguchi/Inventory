bcc784614a8a55924a123f177cbcbae546ebb4f5
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-03-13 19:32:51 -0700

    Added working buttons that focus on the appropiate fields

--- scannerlogic.js
+++ scannerlogic.js
@@ -1,15 +1,28 @@
-var manData = ['hp','dell','apple'];
-function hello (hello) {
-    $('#new').append(hello);
-}
+$(document).ready(function() {
 
-for (var i=0; i < manData.length; i++) {
+    var manData = ['hp','dell','apple'];
+    function hello (hello) {
+        $('#new').append(hello);
+    }
 
-    //creates a label for the manufactuerer and adds to form
-    var label = $('<label class="form-check-label"></label>');
-    label.attr('id','Manu_'+manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
+    for (var i=0; i < manData.length; i++) {
 
-    //creates the actual input radio and adds to the form
-    var manRadio = $('<input class="form-check-input" type="radio" value="checkedValue">');
-    manRadio.attr('id','radio_'+manData[i]).appendTo('#Manu_'+manData[i]);
-}
+        //creates a label for the manufactuerer and adds to form
+        var label = $('<label class="form-check-label"></label>');
+        label.attr('id','Manu_'+manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
+
+        //creates the actual input radio and adds to the form
+        var manRadio = $('<input class="form-check-input" type="radio" value="checkedValue">');
+        manRadio.attr('id','radio_'+manData[i]).appendTo('#Manu_'+manData[i]);
+    }
+
+    //setting up the events on the buttons
+    $('#btnAssetNumber').on('click',function(){
+        $('#assetNumber').focus();
+    });
+
+    $('#btnSerialNumber').on('click',function(){
+        $('#serialNumber').focus();
+    });
+
+});
