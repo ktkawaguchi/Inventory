84759db6752e0a81ea97de2664487a43cb3d43e5
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-03-14 17:32:58 -0700

    Added creation of JSON objects in array when submit is clicked, also clears text fields of data

--- scannerlogic.js
+++ scannerlogic.js
@@ -4,11 +4,7 @@ $(document).ready(function() {
         "manufacturer": "HP",
         "serialNumber":"2U3X12TE2"
     }
-
     var manData = ['HP','Dell','Apple'];
-    function hello (hello) {
-        $('#new').append(hello);
-    }
 
     for (var i=0; i < manData.length; i++) {
 
@@ -18,7 +14,7 @@ $(document).ready(function() {
 
         //creates the actual input radio and adds to the form
         var manRadio = $('<input name="options" autocomplete="off" type="radio" value="checkedValue">');
-        manRadio.attr('id','radio_'+manData[i]).appendTo('#Manu_'+manData[i]);
+        manRadio.attr('id','radio_'+manData[i]).attr('value',manData[i]).appendTo('#Manu_'+manData[i]);
     }
 
     //setting up the events on the buttons
@@ -32,7 +28,28 @@ $(document).ready(function() {
 
     //set up validation of fields
 
-    //create a new JSON object with entered data
+    //create a new JSON object with entered data add it to array of records
+    var recordArray = new Array();
+    $('#btnSubmit').on('click',function(){
+        var valAssetNum = $('#assetNumber').val();
+        var valManufacturer = $("input:radio[name ='options']:checked").val();
+        var valSerialNum = $('#serialNumber').val();
+        console.log(valAssetNum + "  " + valSerialNum + "  " + valManufacturer);
+        createJSON(valAssetNum,valSerialNum,valManufacturer);
+        console.log(recordArray);
+
+        //reset fields
+        $(this).closest('form').find("input[type=text], textarea").val("");
+    });
+
+    function createJSON (asset, serial, manufacturer) {
+        var record = {
+            "AssetNumber" : asset,
+            "SerialNumber" : serial,
+            "Manufacturer" : manufacturer
+        }
+        recordArray.push(record);
+    }
 
 
 });
