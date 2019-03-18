0644c327db9df895b44d2d766e68909f45e2f5da
Ken Kawaguchi <ktkawaguchi@cpp.edu>
2019-03-17 18:59:21 -0700

    Cleaned up the look of the form to be more responsive at different dimensions

--- scannerlogic.js
+++ scannerlogic.js
@@ -1,102 +1,127 @@
 $(document).ready(function() {
-    var recordArray = new Array();
-    var count = 0;
-    var manData = ['HP','Dell','Apple'];
-    var modelDataHP = ['Z230','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3','Elitedesk 800 sff','Elitebook 850 G3'];
-    var modelDataDell = ['Inspiron 3000','Velicity 332'];
-    var modelDataApple = ['iMac','MacBook Pro','iPad'];
+	var recordArray = new Array();
+	var count = 0;
+	var manData = [ 'HP', 'Dell', 'Apple' ];
+	var modelDataHP = [
+		'Z230',
+		'Elitedesk 800',
+		'Elitebook 850 G2',
+		'Z220',
+		'Z240',
+		'Powerbook G1',
+		'Elitebook 850 G1',
+		'Elitedesk 900 sff',
+		'Z250',
+		'PB3432',
+		'Envy 940',
+		'LaserJet 220',
+		'Photosmart 950',
+		'LaserJet 4000',
+		'LaserJet 5000',
+		'PB2322',
+		'All In One 9000',
+		'All In One 8000',
+		'Envy 800',
+		'Envy 700',
+		'Smartbook'
+	];
+	var modelDataDell = [ 'Inspiron 3000', 'Velicity 332' ];
+	var modelDataApple = [ 'iMac', 'MacBook Pro', 'iPad' ];
 
-    //setup for the radio controls
-    for (var i=0; i < manData.length; i++) {
+	//setup for the radio controls
+	for (var i = 0; i < manData.length; i++) {
+		//creates a label for the manufactuerer and adds to form
+		var label = $('<label class="btn btn-info"></label>');
+		label.attr('id', 'Manu_' + manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
 
-        //creates a label for the manufactuerer and adds to form
-        var label = $('<label class="btn btn-info"></label>');
-        label.attr('id','Manu_'+manData[i]).text(manData[i]).appendTo('#manufacturerGroup');
+		//creates the actual input radio and adds to the form
+		var manRadio = $('<input name="manufacturers" autocomplete="off" type="radio">');
+		manRadio.attr('id', 'radio_' + manData[i]).attr('value', manData[i]).appendTo('#Manu_' + manData[i]);
 
-        //creates the actual input radio and adds to the form
-        var manRadio = $('<input name="manufacturers" autocomplete="off" type="radio">');
-        manRadio.attr('id','radio_'+manData[i]).attr('value',manData[i]).appendTo('#Manu_'+manData[i]);
+		//adding event to radio buttons to add model data
+		$('#radio_' + manData[i]).change(function() {
+			$('#modelGroup').empty();
+			createModelGroup(this.value);
+		});
+	}
 
-        //adding event to radio buttons to add model data
-        $('#radio_'+manData[i]).change(function(){
-            $('#modelGroup').empty();
-            createModelGroup(this.value);
-        });
-    }
+	//setting up the events on the buttons
+	$('#btnAssetNumber').on('click', function() {
+		$('#assetNumber').focus();
+	});
+	$('#btnSerialNumber').on('click', function() {
+		$('#serialNumber').focus();
+	});
 
-    //setting up the events on the buttons
-    $('#btnAssetNumber').on('click',function(){$('#assetNumber').focus();});
-    $('#btnSerialNumber').on('click',function(){$('#serialNumber').focus();});
+	//create a new JSON object with entered data add it to array of records
+	$('#btnSubmit').on('click', function() {
+		//need to set up validation of fields
+		var valAssetNum = $('#assetNumber').val();
+		var valManufacturer = $("input:radio[name ='manufacturers']:checked").val();
+		var valSerialNum = $('#serialNumber').val();
+		var valModel = $("input:radio[name ='models']:checked").val();
+		var valString = valAssetNum + '  ' + valSerialNum + '  ' + valManufacturer + ' ' + valModel;
+		console.log(valString);
 
-    //create a new JSON object with entered data add it to array of records
-    $('#btnSubmit').on('click',function(){
+		//update DOM to include new record using tables
+		createJSON(valAssetNum, valSerialNum, valManufacturer, valModel);
+		console.log(recordArray);
 
-        //need to set up validation of fields
-        var valAssetNum = $('#assetNumber').val();
-        var valManufacturer = $("input:radio[name ='manufacturers']:checked").val();
-        var valSerialNum = $('#serialNumber').val();
-        var valModel = $("input:radio[name ='models']:checked").val();
-        var valString = valAssetNum + "  " + valSerialNum + "  " + valManufacturer + " " + valModel;
-        console.log(valString);
+		//reset text input fields
+		$(this).closest('form').find('input[type=text], textarea').val('');
+		$('#assetNumber').focus();
+	});
 
-        //update DOM to include new record using tables
-        createJSON(valAssetNum,valSerialNum,valManufacturer,valModel);
-        console.log(recordArray);
+	//adding selcted manufacturers models to the form
+	function createModelGroup(manuValue) {
+		var selectedManu;
+		$('#modelGroup').attr('style', '');
+		console.log(manuValue);
+		if (manuValue === 'HP') {
+			selectedManu = modelDataHP;
+		} else if (manuValue === 'Dell') {
+			selectedManu = modelDataDell;
+		} else if (manuValue === 'Apple') {
+			selectedManu = modelDataApple;
+		}
 
-        //reset text input fields
-        $(this).closest('form').find("input[type=text], textarea").val("");
-        $('#assetNumber').focus();
-    });
+		for (var i = 0; i < selectedManu.length; i++) {
+			// var spanModel = $('<span class="btn-group-toggle"></span>');
+			// spanModel.attr('id','Span_'+i).appendTo('#modelGroup');
+			var labelModel = $('<label class="btn btn-secondary"></label>');
+			labelModel.attr('id', 'Model_' + i).text(selectedManu[i]).appendTo('#modelGroup');
+			var modelRadio = $('<input name="models" autocomplete="off" type="radio">');
+			modelRadio.attr('id', 'radio_' + i).attr('value', selectedManu[i]).appendTo('#Model_' + i);
+		}
+	}
 
-    //adding selcted manufacturers models to the form
-    function createModelGroup(manuValue) {
-        var selectedManu;
-        $('#modelGroup').attr('style','');
-        console.log(manuValue);
-        if (manuValue==="HP"){selectedManu = modelDataHP;
-        } else if (manuValue==="Dell") {
-            selectedManu = modelDataDell;
-        } else if (manuValue==="Apple") {
-            selectedManu = modelDataApple;
-        }
-        
-        for (var i=0;i<selectedManu.length;i++){
-            // var spanModel = $('<span class="btn-group-toggle"></span>');
-            // spanModel.attr('id','Span_'+i).appendTo('#modelGroup');
-            var labelModel = $('<label class="btn btn-secondary"></label>');
-            labelModel.attr('id','Model_'+i).text(selectedManu[i]).appendTo('#modelGroup');
-            var modelRadio = $('<input name="models" autocomplete="off" type="radio">');
-            modelRadio.attr('id','radio_'+i).attr('value',selectedManu[i])
-                .appendTo('#Model_'+i);
-        }
-    }
+	function createJSON(asset, serial, manufacturer, model) {
+		var record = {
+			ID: count,
+			AssetNumber: asset,
+			SerialNumber: serial,
+			Manufacturer: manufacturer,
+			Model: model
+		};
+		count++;
+		updateDOM(record);
+		recordArray.push(record);
+	}
 
-    function createJSON (asset, serial, manufacturer, model) {
-        var record = {
-            "ID" : count,
-            "AssetNumber" : asset,
-            "SerialNumber" : serial,
-            "Manufacturer" : manufacturer,
-            "Model" : model
-        }
-        count++;
-        updateDOM(record);
-        recordArray.push(record);
-    }
+	function updateDOM(recordUpdate) {
+		$('<tr></tr>').attr('id', 'recordRow' + recordUpdate.ID).appendTo('#recordBody');
+		$('<td></td>').attr('id', 'recordBtn' + recordUpdate.ID).appendTo('#recordRow' + recordUpdate.ID);
+		$('<button type="button" class="btn btn-primary">Delete</button>')
+			.attr('id', 'btn' + recordUpdate.ID)
+			.appendTo('#recordBtn' + recordUpdate.ID);
+		$('<td></td>').text(recordUpdate.AssetNumber).appendTo('#recordRow' + recordUpdate.ID);
+		$('<td></td>').text(recordUpdate.Manufacturer).appendTo('#recordRow' + recordUpdate.ID);
+		$('<td></td>').text(recordUpdate.Model).appendTo('#recordRow' + recordUpdate.ID);
+		$('<td></td>').text(recordUpdate.SerialNumber).appendTo('#recordRow' + recordUpdate.ID);
 
-    function updateDOM (recordUpdate) {
-        $('<tr></tr>').attr('id','recordRow'+recordUpdate.ID).appendTo('#recordBody');
-        $('<td></td>').attr('id','recordBtn'+recordUpdate.ID).appendTo('#recordRow'+recordUpdate.ID);
-        $('<button type="button" class="btn btn-primary">Delete</button>')
-            .attr('id','btn'+recordUpdate.ID).appendTo('#recordBtn'+recordUpdate.ID);
-        $('<td></td>').text(recordUpdate.AssetNumber).appendTo('#recordRow'+recordUpdate.ID);
-        $('<td></td>').text(recordUpdate.Manufacturer).appendTo('#recordRow'+recordUpdate.ID);
-        $('<td></td>').text(recordUpdate.Model).appendTo('#recordRow'+recordUpdate.ID);
-        $('<td></td>').text(recordUpdate.SerialNumber).appendTo('#recordRow'+recordUpdate.ID);
-
-        //button functionality to delete row
-        $('#btn'+recordUpdate.ID).on('click',function(){
-            $('#recordRow'+recordUpdate.ID).remove();
-        });
-    }
+		//button functionality to delete row
+		$('#btn' + recordUpdate.ID).on('click', function() {
+			$('#recordRow' + recordUpdate.ID).remove();
+		});
+	}
 });
